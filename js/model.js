486f120e6623eeea4df923fed854babe2730fac8
will_qiu <will_qiu@innodisk.com>
2022-08-16 12:57:01 +0800

    fix:chagne min, sec of remain time and spend time  in model

--- js/model.js
+++ js/model.js
@@ -292,7 +292,10 @@ function model_info_process(data){
     $("#platform_val").text(PRJ_INFO["front_project"]["platform"]);
     $("#dataset_val").text(data["effect_img_num"]);
     $("#method_val").text(data["training_method"]);
-    $("#time_val").text(data["spend_time"]+"s");
+    // Time processing
+    let min = parseInt(parseInt(data["spend_time"])/60);
+    let sec = parseInt(data["spend_time"])%60;
+    $("#time_val").text(min+"m "+sec+"s");
     // Parameter
     $("#model_val").text(data["model"]);
     $("#gpu_val").text(data["GPU"]).ready(function(){
@@ -312,7 +315,11 @@ function socket_remaining_time(){
         if (MAIN_PAGE=="model" && ITER_NAME == PRJ_INFO["training_info"]["iteration"]){
             if (msg != "0"){
                 $(`#time_text`).text("Remaining time");
-                $('#time_val').text(msg+'s');
+                // Time processing
+                let min = parseInt(parseInt(msg)/60);
+                let sec = parseInt(msg)%60;
+                $("#time_val").text(min+"m "+sec+"s");
+                // $('#time_val').text(msg+'s');
             };
         };  
     });
